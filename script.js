7d14c602cb2270cf261fbc9ec89acaac11e38ffd
Nhlalo <nhlalonkosi@gmail.com>
2022-04-10 14:48:58 +0200

    Add the statistics  block

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const pages = document.querySelector('#pages');
 const published = document.querySelector('#published');
 const read = document.querySelector('#read');
 const bookInforContainer = document.querySelector('.library');
+const additionButton = document.querySelector('.bookAddition');
+const totalBooksDisplay = document.querySelector('.totalBooks');
+const completelyReadBooksDisplay = document.querySelector('.readBooks');
+let totalBooks = 4;
+let completelyReadBooks = 4;
+
+
 
 lightMode.addEventListener('click', () => {
   lightMode.classList.add('FA-visibility');
@@ -21,15 +28,17 @@ darkMode.addEventListener('click', () => {
 const modalWindow = document.querySelector('.modal-window');
 const overLayOut = document.querySelector('.overLayOut')
 
-document.querySelector('.bookAddition').addEventListener('click', () => {
+document.querySelector('.bookAddition').addEventListener('click', e => {
   modalWindow.classList.add('modal-window-visibility');
   overLayOut.classList.add('overLayOut-visibility')
+  e.target.classList.add('bookAddition-visibility')
 
 })
 
 document.querySelector('.fa-close').addEventListener('click', () => {
   modalWindow.classList.remove('modal-window-visibility');
   overLayOut.classList.remove('overLayOut-visibility');
+  document.querySelector('.bookAddition').classList.remove('bookAddition-visibility')
 })
 
 function createBooks(title,author,pages,published,read){
@@ -40,7 +49,12 @@ function createBooks(title,author,pages,published,read){
   this.read = read;
 }
 
-const userLibrary = [];
+const userLibrary = [
+  {title:'The Lord of the Rings', author:'J.R.R Tolkien',pages: '423',published:'1955-10-20',read: 'Read'},
+  {title:'Life of Pi', author:'Yann Martel',pages: '354',published:'2001-09-11',read: 'Read'},
+  {title:'The Great Gatsby', author:'F. Scott Fitzgerald',pages: '163',published:'1925-04-10',read: 'Read'},
+  {title:'The Hobbit', author:'J.R.R Tolkien',pages: '310',published:'1937-09-10',read: 'Read'}
+];
 
 function addBookToLibrary(){
 if(read.checked == true ){
@@ -84,38 +98,57 @@ function addInforDetail(){
 
     if(read.checked == true){
       newCheckbox.click()
-      newCheckbox.checked == true;
+
+      completelyReadBooks++;
+      completelyReadBooksDisplay.textContent = completelyReadBooks;
+    }else{
+      newDiv.setAttribute('id', 'notRead')
+
     }
 
-const inputCheckeds = document.querySelectorAll('.toggleLabel input[type="checkbox"]');
-inputCheckeds.forEach(inputChecked => {
-    const toggleLabelParent = inputChecked.parentElement.parentElement;
-      if(inputChecked.checked == false){
-       toggleLabelParent.setAttribute('id', 'notRead')
-      }else{
-        toggleLabelParent.removeAttribute('id', 'notRead')
-      }
-  })
+const inputChecked = document.querySelector('.toggleLabel input[type="checkbox"]');
 
-inputCheckeds.forEach(inputChecked => {
     const toggleLabelParent = inputChecked.parentElement.parentElement;
-    inputChecked.addEventListener('click', () =>{
-      if(inputChecked.checked == false){
+    inputChecked.addEventListener('click', (e) =>{
+      if(e.target.checked == false){
        toggleLabelParent.setAttribute('id', 'notRead')
-       const tip = userLibrary.findIndex(user => user.title == `${ inputChecked.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent}`)
-       userLibrary[tip].read = 'Not Read';
+       const indexOfArray = userLibrary.findIndex(user => user.title == `${ inputChecked.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent}`)
+       userLibrary[indexOfArray].read = 'Not Read';
+       completelyReadBooks--;
+       completelyReadBooksDisplay.textContent = completelyReadBooks;
+      
       }else{
         toggleLabelParent.removeAttribute('id', 'notRead')
-        const tip = userLibrary.findIndex(user => user.title == `${ inputChecked.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent}`)
-         userLibrary[tip].read = 'Read';
+        const indexOfArray = userLibrary.findIndex(user => user.title == `${ inputChecked.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent}`)
+         userLibrary[indexOfArray].read = 'Read';
+         completelyReadBooks++;
+         completelyReadBooksDisplay.textContent = completelyReadBooks;
+
       }
     }
     )
-  })
 
-    fontAwesomeClose.addEventListener('click', () => {
-      bookInforContainer.removeChild(fontAwesomeClose.parentElement)
+
+   const faClose =  document.querySelector('.fa-close2');
+  
+     faClose.addEventListener('click', () => {
+      console.log(faClose.nextElementSibling)
+      bookInforContainer.removeChild(faClose.parentElement)
+      const indexOfArray = userLibrary.findIndex(user => user.title == `${faClose.nextElementSibling.textContent}`)
+      userLibrary.splice(indexOfArray,1)
+      totalBooks--;
+      totalBooksDisplay.textContent = completelyReadBooks;
+      if(e.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.checked == true){
+        completelyReadBooks--;
+        completelyReadBooksDisplay.textContent = completelyReadBooks;
+     }
+     
+     
     })
+
+
+ 
+    
   
 }
 
@@ -125,35 +158,58 @@ document.querySelector('.newBook').addEventListener('click', e => {
  if(title.value.length > 0 && author.value.length > 0 && !isNaN(pages.value) && published.value.length > 0){
   addBookToLibrary()
   addInforDetail()
+  totalBooks++;
+  totalBooksDisplay.textContent = totalBooks;
+  document.querySelector('.bookAddition').classList.remove('bookAddition-visibility')
 }
+console.log(userLibrary)
   title.value = ''
   author.value = '';
   pages.value = '';
   published.value = '';
   read.checked = false;
- 
 })
 
 const inputCheckeds = document.querySelectorAll('.toggleLabel input[type="checkbox"]');
 inputCheckeds.forEach(inputChecked => {
     const toggleLabelParent = inputChecked.parentElement.parentElement;
-    inputChecked.addEventListener('click', () =>{
-      if(inputChecked.checked == false){
-       toggleLabelParent.setAttribute('id', 'notRead')
+    inputChecked.addEventListener('click', (e) =>{
+      if(e.target.checked == false){
+        toggleLabelParent.setAttribute('id', 'notRead')
+        const indexOfArray = userLibrary.findIndex(user => user.title == `${ inputChecked.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent}`)
+        userLibrary[indexOfArray].read = 'Not Read';
+        completelyReadBooks--;
+        completelyReadBooksDisplay.textContent = completelyReadBooks;
+     
       }else{
         toggleLabelParent.removeAttribute('id', 'notRead')
+        const indexOfArray = userLibrary.findIndex(user => user.title == `${ inputChecked.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent}`)
+         userLibrary[indexOfArray].read = 'Read';
+         completelyReadBooks++;
+         completelyReadBooksDisplay.textContent = completelyReadBooks;
+     
       }
     }
     )
   })
 
+ 
   document.querySelectorAll('.fa-close2').forEach( deleteBookInfor => {
     const bookInforDeletion = deleteBookInfor.parentElement;
-    deleteBookInfor.addEventListener('click', () => {
+    deleteBookInfor.addEventListener('click', (e) => {
       bookInforContainer.removeChild(bookInforDeletion)
+      const indexOfArray = userLibrary.findIndex(user => user.title == `${deleteBookInfor.nextElementSibling.textContent}`)
+      userLibrary.splice(indexOfArray,1)
+      totalBooks--;
+      totalBooksDisplay.textContent = totalBooks;
+      if(deleteBookInfor.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.checked == true){
+         completelyReadBooks--;
+         completelyReadBooksDisplay.textContent = completelyReadBooks;
+      }
     })
   
   })
 
  
- 
+
+
